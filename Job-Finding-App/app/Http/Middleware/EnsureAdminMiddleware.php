7dbc2f9d6eddb71e1333570e2ccdbbbcc3a6db06
Shuprawal <shuprawalkarki121@gmail.com>
2025-05-05 11:17:28 +0545

    new new csrf

---
+++ Job-Finding-App/app/Http/Middleware/EnsureAdminMiddleware.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Facades\ApiResponse;
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class EnsureAdminMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $user = $request->user();
+
+        if (!$user || $user->role !== 'admin' ){
+            return ApiResponse::error('Unauthorized', Response::HTTP_UNAUTHORIZED);
+        }
+
+        return $next($request);
+    }
+}
--- Job-Finding-App/bootstrap/app.php
+++ Job-Finding-App/bootstrap/app.php
@@ -22,6 +22,7 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
         $middleware->api(append: [
             \App\Http\Middleware\EnsureAPIResult::class,
+            \Illuminate\Foundation\Http\Middleware\VerifyCsrfToken::class,
         ]);
 
         $middleware->alias([
